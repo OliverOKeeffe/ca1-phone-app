39af430eade1af7e8d0811fff7d7db9aa3a60d96
Oliver O'Keeffe <oliverokeeffe@Olivers-MacBook-Air.local>
2023-11-04 14:09:09 +0000

    Made the retailer controller

---
+++ app/Http/Controllers/RetailerController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Retailer;
+
+class RetailerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $retailers = Retailer::orderBy('created_at', 'desc')->paginate(8);
+
+        return view('retailers.index', [
+            'retailers' => $retailers 
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('retailers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rules =[
+            'name'=> 'required|string|unique:retailerss,name|min:2|max:150',
+            'founded'=> 'required|string|min:4|max:4',
+            'num_location'=> 'required|string|min:1|max:100',
+
+        ];
+
+        $messages=[
+            'retailer.unique'=>'Retailer name should be unique'
+        ];
+
+        $request->validate($rules, $messages);
+
+
+        $retailer = new Retailer;
+        $retailer->name = $request->name;
+        $retailer->founded = $request->founded;
+        $retailer->num_location = $request->num_location;
+        $retailer->save();
+
+        return redirect()->route('retailers.index')->with('status', 'Created a new Retailer');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $retailer = Retailer::findOrFail($id);
+        return view('retailers.show', [
+            'retailer' => $retailer
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $retailer = Retailer::findOrFail($id);
+        return view('retailer.edit', [
+            'retailer' => $retailer
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $rules =[
+            'name'=> "required|string|unique:retailers,name,{$id}|min:3|max:50",
+            'founded'=> 'required|string|min:4|max:4',
+            'num_location'=> 'required|string|min:5|max:1000',
+
+        ];
+
+        $messages=[
+            'name.unique'=>'Retailer title should be unique'
+        ];
+
+        $request->validate($rules, $messages);
+
+
+        $retailer = Brand::findOrFail($id);
+        $retailer->name = $request->name;
+        $retailer->founded = $request->founded;
+        $retailer->num_location = $request->num_location;
+        $retailer->save();
+
+        return redirect()       
+            ->route('retailers.index')
+            ->with('status', ' Updated a Retailer');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $reatiler = Retailer::findOrFail($id);
+        $retailer->delete();
+
+        return redirect()->route('retailers.index')->with('status', 'Retailer deleted successfully');
+    }
+}
