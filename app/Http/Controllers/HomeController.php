4b60aa0efede05441bac2997114dbff9cba0ac07
Oliver O'Keeffe <oliverokeeffe@Olivers-Air.station>
2023-12-03 17:14:24 +0000

    Created the home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $user = Auth::user();
+
+        $home = 'home';
+
+        if($user->hasRole('admin')){
+            //do some admin stuff
+            $home = 'admin.home';
+        }
+        else if($user->hasRole('user')){
+                //do some user stuff
+                $home = 'user.home';
+        }
+
+        return view($home);
+        // return redirect()->route($home);
+            
+    }
+}
