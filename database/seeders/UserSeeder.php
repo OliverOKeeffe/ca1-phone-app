edb9a346002a09f3461e5f91cb874a1b8048f4f9
Oliver O'Keeffe <oliverokeeffe@Olivers-Air.station>
2023-12-03 16:58:30 +0000

    Updated the user seeder by adding a admin and regular user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,10 +14,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = new user;
+        $role_admin = Role::where('name', 'admin')->first();
+
+        $role_user = Role::where('name', 'user')->first();
+
+
+
+        $admin = new User;
         $admin->name = "Oliver O'Keeffe";
-        $admin->email = "n00221912@iadt.ie";
+        $admin->email = "admin@ca1example.com";
         $admin->password = "secret123";
         $admin->save();
+
+        //attach admin role to the user created above
+        $admin->roles()->attach($role_admin);
+
+        $user = new User;
+        $user->name = "John Jones";
+        $user->email = "user@ca1example.com";
+        $user->password = "secret123";
+        $user->save();
+
+        $user->roles()->attach($role_user);
+
+
+
     }
 }
